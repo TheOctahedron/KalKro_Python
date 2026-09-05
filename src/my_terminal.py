de0396faa02a8dacb6229c7bf5d20dfe14f22f74
Octahedron <octahedron929@gmail.com>
2026-09-05 19:38:57 +0300

    refactor: clean up unused code in find_command()
    clean up in find_command() (my_terminal.py):
    - (remove) unused 'answer' variable
    - (remove) debug 'print(f"\n{answer}")'

--- src/my_terminal.py
+++ src/my_terminal.py
@@ -6,11 +6,10 @@ b = "\n[YOU]: "
 def my_terminal():
     print(f"{a}Welcome to KalKro.")
     while True:
-        cmd = input(f"{a}Press F to open File Explorer.{b}").strip().upper()
+        cmd = input(f"\n{a}Press F to open File Explorer.{b}").strip().upper()
         match cmd:
             case "F":
                 pass
             case _:
-                answer = find_command(cmd)
-                print(answer)
+                find_command(cmd)
                 continue
