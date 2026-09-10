ffcfae20eedb79738d1e051b6230419e66db3bac
Octahedron <octahedron929@gmail.com>
2026-09-10 20:26:13 +0300

    refactor: added '.lower().strip()'
    Added '.lower().strip()' to var 'answer' for versatility and error avoidance.
    - 'answer = input("\n> ")' to 'answer = input("\n> ").lower().strip()'

--- src/utilities/user_input.py
+++ src/utilities/user_input.py
@@ -2,7 +2,7 @@ def user_input(requirement):
     print("\n\nEnter 0 To Exit.\n")
     while True:
         print(requirement)
-        answer = input("\n> ")
+        answer = input("\n> ").lower().strip()
         
         match answer:
             case "0":
