6a99af8e688cae488a7247b49ff49609adf3b6ab
Octahedron <octahedron929@gmail.com>
2026-09-10 17:59:15 +0300

    refactor: standardize command format to .lower()
    .lower() is much clearer and more readable than.upper(). 
    Therefore, it is now accepted for commands and other inputs.

--- src/terminal/my_terminal.py
+++ src/terminal/my_terminal.py
@@ -6,9 +6,9 @@ b = "\n[YOU]: "
 def my_terminal():
     print(f"{a}Welcome to KalKro.")
     while True:
-        cmd = input(f"\n{a}Press F to open File Explorer.{b}").strip().upper()
+        cmd = input(f"\n{a}Press F to open File Explorer.{b}").strip().lower()
         match cmd:
-            case "F":
+            case "f":
                 pass
             case _:
                 find_command(cmd)
