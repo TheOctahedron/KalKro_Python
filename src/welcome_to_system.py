75d7963847bda4a4116153793cb0f2e042c055b9
Octahedron <octahedron929@gmail.com>
2026-09-12 19:21:02 +0300

    refactor: add main guard & welcome function
    Two pleasant additions have been made:
    - (def welcome_to_system()) The welcome for the system is now in the function.
    - (__name__ == "__main__") Added main guard.

--- src/welcome_to_system.py
+++ src/welcome_to_system.py
@@ -1,4 +1,8 @@
 from my_desktop import my_desktop
-print("Welcome to KalKro Python.")
-input("\nPRESS ENTER TO ENTER THE DESKTOP ")
-my_desktop()
+def welcome_to_system():
+    print("Welcome to KalKro Python.")
+    input("\nPRESS ENTER TO ENTER THE DESKTOP ")
+    my_desktop()
+
+if __name__ == "__main__":
+    welcome_to_system()
