4f2e32490b5538c37e1b012e0ce05119092945af
Octahedron <octahedron929@gmail.com>
2026-09-16 23:21:03 +0300

    upd: added two new commands
    added two new commands:
    - (!show selected folder!) Added.
    - (!enter id!) Added.

--- src/terminal/commands/commands_storage.py
+++ src/terminal/commands/commands_storage.py
@@ -1,11 +1,15 @@
-from terminal.commands.commands_functions import hello_world, about_kalkro, random_100
+from terminal.commands.commands_functions import hello_world, about_kalkro, random_100, show_selected_folder, enter_id
+
 
 system_commands = [
-    {"command": "!hello world!", "function": hello_world},
-    {"command": "!about kalkro!", "function": about_kalkro},
-    {"command": "!random 100!", "function": random_100}
+    {"command": "!hello world!", "function": hello_world.hello_world},
+    {"command": "!about kalkro!", "function": about_kalkro.about_kalkro},
+    {"command": "!random 100!", "function": random_100.random_100},
+    {"command": "!show selected folder!", "function": show_selected_folder.show_selected_folder},
+    {"command": "!enter id!", "function": enter_id.enter_id}
 ]
 
 def help_me():
     for number, system_command in enumerate(system_commands, 1):
         print(f"{number} {system_command['command']}")
+        
