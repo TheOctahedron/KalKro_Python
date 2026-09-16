286d650b822a279a572ae48ff700fc671538a796
Octahedron <octahedron929@gmail.com>
2026-09-16 21:50:28 +0300

    fix: tags fixed
    The tags were mixed up. 
    ... They have been corrected, and a tag explanation has been added 
    ... (this will always be included during import so it doesn't get forgotten).

--- src/terminal/terminal_actions/get_command.py
+++ src/terminal/terminal_actions/get_command.py
@@ -1,19 +1,24 @@
 from utilities.terminal_utilities.terminal_tags import a, b
+"""
+a = [TERMINAL]:
+b = [YOU]:
+"""
+
 from utilities.terminal_utilities.terminal_errors import terminal_errors
 from terminal.terminal_actions.find_command import find_command
 
 def get_command(obj, cmd): # object, command
     if obj is None:
-        print(f"{a}OBJECT: None")
+        print(f"{b}OBJECT: None")
     else:
-        print(f"{a}OBJECT: Added")
+        print(f"{b}OBJECT: Added")
 
 
     if cmd is None:
-        print(f"{a}COMMAND: None")
-        return f"{b}{terminal_errors[0]['command_errors'][2]}" # ERROR: Command is None.
+        print(f"{b}COMMAND: None")
+        return f"{a}{terminal_errors[0]['command_errors'][2]}" # ERROR: Command is None.
     else:
-        print(f"{a}COMMAND: {cmd}")
+        print(f"{b}COMMAND: {cmd}")
 
     
     if obj is None and cmd is not None:
