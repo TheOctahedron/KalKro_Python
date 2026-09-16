605db44212d44d854cdef1a98e8861e7c4010e8a
Octahedron <octahedron929@gmail.com>
2026-09-16 20:15:08 +0300

    upd: structure updated: compartments added.
    The structure of the variable storing information about all errors encountered in the terminal has been updated.
    - (Categories have been added); errors will now be sorted by category (e.g., Commands, Objects).

--- src/utilities/terminal_utilities/terminal_errors.py
+++ src/utilities/terminal_utilities/terminal_errors.py
@@ -1,4 +1,10 @@
-terminal_errors = {
-    1: "ERROR: Command is not Found.",
-    2: "ERROR: Command is None."
-}
+terminal_errors = [{
+    "command_errors": {
+        1: "ERROR: Command is not Found.",
+        2: "ERROR: Command is None."
+    },
+    "object_errors": {
+        1: "ERROR: Object is not Found.",
+        2: "ERROR: Required Object is None."
+    }
+}]
