fa7a4c96d1c28db6fef5c54a6b49f0fc39140aeb
Octahedron <octahedron929@gmail.com>
2026-09-17 22:52:09 +0300

    chore: add 'file_actions/' & 1 file (file-action)
    A 'file_actions/' folder has been added, along with the one function inside it:
    - (file_actions/); Folder to store file-related functions.
    - (file_actions/show_files.py); A function that show all files in the system.

---
+++ src/file_explorer/explorer_actions/file_actions/show_files.py
@@ -0,0 +1,7 @@
+from utilities.explorer_utilities.folders import folders
+
+def show_files(selected_folder):
+    print(f"SELECTED FOLDER: {selected_folder['name']}")
+    for file in selected_folder['entries']:
+        print(f"{file['name']}{file['type']}")
+    return # The function is incomplete.
