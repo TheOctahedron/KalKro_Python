ba7d3d4c67d61304f94503f2f5f849fe354458d4
Octahedron <octahedron929@gmail.com>
2026-09-17 22:37:44 +0300

    chore: added 'folder_actions/' & 2 folder-actions
    Added 1 folder and 2 actions inside:
    - (folder_actions/); Folder containing functions for the folder.
    - (folder_actions/select_folder.py); A function that allows selecting a folder.
    - (folder_actions/show_folders.py); A function that displays all folders in the system.

---
+++ src/file_explorer/explorer_actions/folder_actions/select_folder.py
@@ -0,0 +1,34 @@
+from utilities.user_input import user_input
+from utilities.explorer_utilities.folders import folders
+
+def select_folder():
+    """
+    We ask for the number of the selected folder from the list.
+    """
+    while True:
+        selected_folder = user_input("Write the number of the Selected Folder.")
+        if selected_folder == 0:
+            return 0
+        
+        try:
+            selected_folder = int(selected_folder)
+        except Exception as e:
+            print(e)
+            continue
+
+
+        found = False
+
+        for number, folder in enumerate(folders, 1):
+
+            if selected_folder == number:
+                selected_folder = folder
+                found = True
+
+        if found == False:
+            print("Folder is Not Found.")
+            continue
+
+        break
+    
+    return selected_folder
---
+++ src/file_explorer/explorer_actions/folder_actions/show_folders.py
@@ -0,0 +1,14 @@
+from utilities.explorer_utilities.folders import folders
+
+def show_folders():
+    while True: 
+        if folders == None:
+            print("You don't have any folders.")
+            input("Press Enter To Exit.")
+            return
+        print("\n\nFOLDERS:")
+        for number, folder in enumerate(folders, 1):
+            print(f"[{number}] {folder['name']}")
+        print("\nend of folders.")
+        break
+    return
