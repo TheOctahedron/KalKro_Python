01a1bd88da7ecb06b8c361b2c6449c61444ca95f
Octahedron <octahedron929@gmail.com>
2026-09-18 17:03:12 +0300

    upd & rename: from commands_storage.py to system_commands.py
    - (rename); from "commands_storage.py" to "system_commands.py"
    - (upd); added "!show_id!" (instead of "!show selected folder!"

--- src/terminal/commands/system_commands.py
+++ src/terminal/commands/system_commands.py
@@ -1,11 +1,11 @@
-from terminal.commands.commands_functions import hello_world, about_kalkro, random_100, show_selected_folder, enter_id
+from terminal.commands.commands_functions import hello_world, about_kalkro, random_100, show_id, enter_id
 
 
 system_commands = [
     {"command": "!hello world!", "function": hello_world.hello_world},
     {"command": "!about kalkro!", "function": about_kalkro.about_kalkro},
     {"command": "!random 100!", "function": random_100.random_100},
-    {"command": "!show selected folder!", "function": show_selected_folder.show_selected_folder},
+    {"command": "!show id!", "function": show_id.show_id},
     {"command": "!enter id!", "function": enter_id.enter_id}
 ]
 
