ec60ce220df566ae641921ad066f75a7a3f35d0c
Octahedron <octahedron929@gmail.com>
2026-09-20 22:47:28 +0300

    remove legacy

--- src/utilities/explorer_utilities/folders.py
+++ src/utilities/explorer_utilities/folders.py
@@ -18,20 +18,4 @@
 }
 """
 
-folders = [{
-"name": "Name",
-"type": ".folder",
-"id": "123",
-
-"entries": [
-    {
-        "name": "hello", 
-        "type": ".txt", 
-        "id": "321",
-
-        "content": [
-            "Hello, World."
-        ]
-    }
-]
-}]
+folders = []
