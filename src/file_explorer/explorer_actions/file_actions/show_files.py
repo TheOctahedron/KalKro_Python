7ca4b2176d9bbddc373679504de341ff76b2a615
Octahedron <octahedron929@gmail.com>
2026-09-20 22:50:30 +0300

    fix: cycle 'for ... in ...' is fixed.
    - (fix); "for file in selected_folder['entries']" to "for file in selected_folder['content']"

--- src/file_explorer/explorer_actions/file_actions/show_files.py
+++ src/file_explorer/explorer_actions/file_actions/show_files.py
@@ -2,6 +2,6 @@ from utilities.folders import folders
 
 def show_files(selected_folder):
     print(f"SELECTED FOLDER: {selected_folder['name']}")
-    for file in selected_folder['entries']:
+    for file in selected_folder['content']:
         print(f"{file['name']}{file['type']}")
     return # The function is incomplete.
