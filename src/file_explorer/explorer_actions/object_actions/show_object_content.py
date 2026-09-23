5ebc30a1b335f6d0b9136ad6dbeb891647ac528b
Octahedron <octahedron929@gmail.com>
2026-09-23 22:48:26 +0300

    refactor: simplify and rename to show_object_content.
    Great refactoring: the function has been renamed to a more descriptive name and simplified, removing unnecessary code.

--- src/file_explorer/explorer_actions/object_actions/show_object_content.py
+++ src/file_explorer/explorer_actions/object_actions/show_object_content.py
@@ -1,17 +1,15 @@
-from utilities.folders import folders
-
-def object_content(selected_object):
+def show_object_content(selected_object):
     print(f"SELECTED OBJECT: {selected_object['name']}")
+
     if selected_object['type'] is ".folder":
-        print("\nFOLDER CONTENT: ")
+        print(f"\n{selected_object['name']} CONTENT: ")
         for number, obj in enumerate(selected_object['content']):
             print(f"[{number}] {obj['name']}{obj['type']}")
-            print("\nEND OF FOLDER-CONTENT.")
-        print(f"{obj['name']}{obj['type']}")
+            print(f"\nEND OF {selected_object['name']}.")
         
     for file_content in selected_object['content']:
         print(f"\nFILE CONTENT: ")
         print(file_content)
         print("\nEND OF FILE-CONTENT.")
     
-    return 
+    return
