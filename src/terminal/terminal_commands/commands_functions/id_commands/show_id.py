684547fe9c9dee90dfa2239cc225f9995ceea0aa
Octahedron <octahedron929@gmail.com>
2026-09-23 22:20:35 +0300

    style: 'object' is shorting to 'obj'.

--- src/terminal/terminal_commands/commands_functions/id_commands/show_id.py
+++ src/terminal/terminal_commands/commands_functions/id_commands/show_id.py
@@ -4,7 +4,7 @@ from utilities.terminal_utilities.terminal_errors import terminal_errors
 def show_id(obj_name, storage):
     if obj_name == None:
         return terminal_errors[0]['object_errors'][2]
-    for object in storage:
-        if object['name'] == obj_name:
-            return f"ID: {object['id']}"
+    for obj in storage:
+        if obj['name'] == obj_name:
+            return f"ID: {ob['id']}"
     return "Object is not Found."
