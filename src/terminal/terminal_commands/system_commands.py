20aa3b3290f80a1d0e94743e1339ec1e67438b1a
Octahedron <octahedron929@gmail.com>
2026-09-23 19:09:08 +0300

    fix: import from terminal_commands fixed.

--- src/terminal/terminal_commands/system_commands.py
+++ src/terminal/terminal_commands/system_commands.py
@@ -1,6 +1,6 @@
-from terminal.commands.commands_functions import hello_world, about_kalkro, random_100
+from terminal.terminal_commands.commands_functions import hello_world, about_kalkro, random_100
 
-from terminal.commands.commands_functions.id_commands import show_id, enter_id
+from terminal.terminal_commands.commands_functions.id_commands import show_id, enter_id
 
 
 system_commands = [
